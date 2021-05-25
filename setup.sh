8a4aea3f745e09c31d4a2cceba57f01be89a884e
Manish Kumar <manishkumar.sharma0103@gmail.com>
2021-05-25 10:34:31 +0530

    Update

--- setup.sh
+++ setup.sh
@@ -2,7 +2,7 @@
 mkdir -p ~/.streamlit/
 echo "\
 [general]\n\
-email = \"your-email@domain.com\"\n\
+email = \"manishkumar.sharma0103@gmail.com\"\n\
 " > ~/.streamlit/credentials.toml
 echo "\
 [server]\n\
